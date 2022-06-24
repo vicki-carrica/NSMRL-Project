fdead9ab072a3a7946cc5eae8e117c9a9e6732e0
Vicki <108072989+vicki-carrica@users.noreply.github.com>
2022-06-24 16:33:28 -0400

    Update draft.java

--- draft.java
+++ draft.java
@@ -13,13 +13,20 @@ public class main {
 		int unfitSurv;
 		int candles;
 		int canisters;
-		double presFSW;
+		double pressure;
 		int aftVol;
 		int flood;
 		int percOx;
-		double conc; //concentration? how do you find that? const?
+		double oConc; //concentration of oxygen? how do you find that? const?
+		double coConc;
 		double oSurvTime;
+		double coSurvTime;
 		double remainingHr; 
+		double temp;
+		double coSET;
+		double oSET;
+		double presATA;
+		double vBreath;
 		/*
 		 * Define more variables here
 		 */
@@ -34,44 +41,83 @@ public class main {
 		candles = scan.nextInt();
 		System.out.print("Input the number of ExtendAir kits in the submarine: ");
 		canisters = scan.nextInt();
-		System.out.print("Input the pressure (in fsw) in the submarine: ");
-		presFSW = scan.nextDouble();
+		System.out.print("Input the pressure (in fsw) in the submarine: "); //idk what unit pressure is in? fsw?
+		pressure = scan.nextDouble();
 		System.out.print("Input the percentage of the submarine compartment which is flooded: ");
 		flood = scan.nextInt();
-		System.out.print("Input the initial concentration of oxygen: "); //ask jeff
-		conc = scan.nextDouble();
-		oSurvTime = oxSurvTime(fitSurv, unfitSurv, candles, presFSW, flood, conc);
+		System.out.print("Input the temperature (in F) in the submarine: ");
+		temp = scan.nextDouble();
+		System.out.print("Input the initial concentration (in %SEV) of oxygen: "); //ask
+		oConc = scan.nextDouble();
+		System.out.print("Input the initial concentration (in %SEV) of carbon dioxide: "); //ask 
+		coConc = scan.nextDouble();
+		oSurvTime = oxSurvTime(fitSurv, unfitSurv, candles, flood, oConc, temp);
 		System.out.println("Approximate survival time (O2) if depth prevents escape: " + oSurvTime); //not SEV, survival time instead, dont know what the relationship is yet
+		coSurvTime = coSurvTime(canisters, fitSurv, unfitSurv, coConc, flood, temp); //wrong value, by a lot
+		System.out.println("Approximate survival time (CO2) if depth prevents escape: " + coSurvTime);
+		remainingHr = hourBreathing(fitSurv, unfitSurv); //wrong value, by a lot
+		System.out.println("Approximate remaining escaper hours of breathing after the start of escape: " + remainingHr);
+		presATA = fswToATA(pressure);
+		vBreath = calcVBreath(flood, fitSurv, presATA);
+		System.out.println("Approximate Vbreath: " + vBreath);
+		oSET = oStartEscapeTime(candles, fitSurv, unfitSurv, flood, oConc, vBreath, temp, remainingHr);
+		System.out.println("Approximate O2 Start Escape Time: " + oSET);
+		coSET = coStartEscapeTime(canisters, fitSurv, unfitSurv, flood, coConc, vBreath, temp, remainingHr);
+		System.out.println("Approximate CO2 Start Escape Time: " + coSET);
+		if (oSET > coSET) {
+			System.out.println("Time to escape: " + oSET);
+		} else {
+			System.out.println("Time to escape: " + coSET);
+		}
 		
-		
-
 	}
 	
-	static double oxSurvTime(int fitSurv, int unfitSurv, int candles, double presFSW, int flood, double conc) {
+	static double oxSurvTime(int fitSurv, int unfitSurv, int candles, int flood, double oConc, double temp) { //is temp needed?
 		/*
 		 * Function calculates approximate survival time based on oxygen if depth prevents escape
 		 */
 		int fit = fitSurv;
 		int unfit = unfitSurv;
 		int cand = candles;
-		double pres = presFSW;
 		int percentFlood = flood;
-		double oxygenConcentration = conc;
+		double oxygenConcentration = oConc/100.0;
+		double temperature = temp + 459.67;
 		int survivors = fit + unfit; //ask if the calculations want total survivors or fit?
 		//volume calculation:
-		double volumeCompt = (100-percentFlood)*79800/100;
+		double volumeCompt = (100-percentFlood)*62800/100;
 		//candle time calculation:
 		int candleSCF = cand * 115; //unit: SCF
 		int survivorSCF = survivors; //unit: SCF/hr
 		double tOCandles = (double)candleSCF/(double)survivorSCF; //unit: hrs
 		//O2 time calculation:
-		double numerator = (1.0/.7302)*volumeCompt*(oxygenConcentration-0.13);
-		double denominator = .0838/32;
+		double numerator = (1.0/.7302)*volumeCompt*(oxygenConcentration-0.13)*(1.0/temperature);
+		double denominator = (survivors*.0838)/32;
 		double tOThirteen = numerator/denominator;
 		//total survival time calculation
-		double OSurvivalTime = tOCandles - tOThirteen;
+		double OSurvivalTime = tOCandles + tOThirteen;
 		
 		return OSurvivalTime;
+	}
+	
+	static double coSurvTime(int canisters, int fitSurv, int unfitSurv, double coConc, int flood, double temp) {
+		/*
+		 * Function calculates approximate survival time based on carbon dioxide if depth prevents escape
+		 */
+		int fit = fitSurv;
+		int unfit = unfitSurv;
+		int cani = canisters;
+		int percentFlood = flood;
+		double coConcentration = coConc/100.0;
+		double temperature = temp + 459.67;
+		double volumeCompt = (100-percentFlood)*62800/100;
+		int survivors = fit + unfit;
+		double tLiOH = (cani*73)/survivors;
+		double numerator = (1.0/.7302)*volumeCompt*(.06 - coConcentration)*(1.0/temperature);
+		double denominator = (survivors * 0.1)/44;
+		double tCO = numerator/denominator;
+		
+		double coSurvivalTime = tLiOH + tCO;
+		return coSurvivalTime;
 	}
 	
 	static double hourBreathing(int fitSurv, int unfitSurv) {
@@ -88,5 +134,69 @@ public class main {
 		double g = escaperHr + nonEscaperHr;
 		return g;
 	}
+	
+	static double calcVBreath(int flood, int fitSurv, double presATA) {
+		/*
+		 * Calculates the volume (Vbreath) in the equations
+		 */
+		int percentFlooded = flood;
+		int fit = fitSurv;
+		double pressure = presATA;
+		double vCompt = (100-percentFlooded)* pressure* 62800/100; //this equation is different for vcompt than before
+		double vBreath = vCompt - 214 - (fit*53.5);
+		return vBreath;
+	}
+	
+	static double fswToATA(double pressure) {
+		/*
+		 * Converts pressure from fsw to ATA
+		 */
+		double fsw = pressure;
+		double ata = (fsw+33)/33;
+		return ata;
+	}
+	static double oStartEscapeTime(int candles, int fitSurv, int unfitSurv, int flood, double oConc, double vBreath, double temp, double g) {
+		/*
+		 * Function that calculates the start escape time based on the O2 
+		 */
+		int fit = fitSurv;
+		int unfit = unfitSurv;
+		int cand = candles;
+		int percentFlood = flood;
+		double concInt = oConc/100.0;
+		double volumeBreath = vBreath;
+		double breathingHr = g;
+		double temperature = temp + 459.67;
+		double volumeCompt = (100-percentFlood)*62800/100;
+		int survivors = fit + unfit;
+		double tCandles = ((double)cand*115.0)/(double)survivors;
+		double numerator = ((concInt*volumeCompt)-(.13*volumeBreath))*(1/.7302)*(1/temperature)-breathingHr*.0838*(1/32);
+		double denominator = (double)survivors*.0838*(1/32.0);
+		double oWaitTime = numerator/denominator;
+		double oSET = oWaitTime+tCandles;
+		return oSET;
+	}
+	static double coStartEscapeTime(int canisters, int fitSurv, int unfitSurv, int flood, double coConc, double vBreath, double temp, double g) {
+		/*
+		 * Function that calculates the start escape time based on the CO2 
+		 */
+		int fit = fitSurv;
+		int unfit = unfitSurv;
+		int cani = canisters;
+		int percentFlood = flood;
+		double concInt = coConc/100.0;
+		double volumeBreath = vBreath;
+		double breathingHr = g;
+		double temperature = temp + 459.67;
+		double volumeCompt = (100-percentFlood)*62800/100;
+		int survivors = fit + unfit;
+		double tLiOH = (cani*73)/survivors;
+		double numerator = ((.06*volumeBreath)-(concInt*volumeCompt))*(1/.7302)*(1/temperature)-breathingHr*.1*(1/44);
+		double denominator = (double)survivors*.1*(1.0/44.0);
+		double coWaitingTime = numerator/denominator;
+		double coSET = coWaitingTime + tLiOH;
+		return coSET;
+	}
+	
 
 }
