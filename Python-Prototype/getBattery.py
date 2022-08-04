35849640e2dfec02b70cbdf1c2c84dad84a822d7
Robro1878 <roal1878@gmail.com>
2022-08-04 09:45:33 -0400

    Fix for code not starting on startup

--- Python-Prototype/getBattery.py
+++ Python-Prototype/getBattery.py
@@ -10,28 +10,35 @@ PORT = 8423
 
 
 #Gets Battery percentage from pisugar api, converts into float, and truncates decimals.
+
 def GetBattery():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
-        s.sendall(bytes("get battery", 'utf-8'))
-        data = s.recv(1024)
-    data = data.decode('utf-8')
-    data = data.replace("battery: ",'')
-    data = data.replace("\n",'')
-    data = float(data)
-    data = math.trunc(data)
-    return data 
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST,PORT))
+            s.sendall(bytes("get battery", 'utf-8'))
+            data = s.recv(1024)
+        data = data.decode('utf-8')
+        data = data.replace("battery: ",'')
+        data = data.replace("\n",'')
+        data = float(data)
+        data = math.trunc(data)
+        return data 
+    except ConnectionRefusedError:
+        return 0
 
 #Returns True if battery is plugged into powersource, returns false if it isn't
 def GetBatteryPlugged():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
-        s.sendall(bytes("get battery_power_plugged", 'utf-8'))
-        data = s.recv(1024)
-    data = data.decode('utf-8')
-    data = data.replace("battery_power_plugged: ", '')
-    data = data.replace("\n",'')
-    if data == "true":
-        return True
-    else:
-        return False 
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST,PORT))
+            s.sendall(bytes("get battery_power_plugged", 'utf-8'))
+            data = s.recv(1024)
+        data = data.decode('utf-8')
+        data = data.replace("battery_power_plugged: ", '')
+        data = data.replace("\n",'')
+        if data == "true":
+            return True
+        else:
+            return False 
+    except ConnectionRefusedError:
+        return False
